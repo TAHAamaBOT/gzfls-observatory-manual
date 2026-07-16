acaf76e49bbb4b1152fbd11672e9e057d6bd0236
TAHAamaBOT <3089124325@qq.com>
2026-07-16 15:05:15 +0800

    chore: 将「天文社」更名为「天文协会」

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={`欢迎来到${siteConfig.title}`}
-      description="广州外国语学校译星者天文社，英文名“Stellar Decoder”，我们在校内开展天文科普和天台观星，在校外积极参与各校天文社团联谊，打造学术规范严谨而富有人文关怀的广外品牌社团。">
+      description="广州外国语学校译星者天文协会，英文名“Stellar Decoder”，我们在校内开展天文科普和天台观星，在校外积极参与各校天文社团联谊，打造学术规范严谨而富有人文关怀的广外品牌社团。">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
